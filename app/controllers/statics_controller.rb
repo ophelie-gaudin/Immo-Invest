fd33765f3f69e43e17109e4e13c2305af228595b
Mariem <Mariem2708@gmail.com>
2021-12-07 19:13:57 +0100

    contact mailer

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,5 +1,14 @@
 class StaticsController < ApplicationController
+  after_contact :admin_email
+
   def contact
     
   end
+
+  def admin_send
+    UserMailer.admin_email(self).deliver_now
+  end
+
+
+
 end
