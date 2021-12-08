9b5128a656fba774c68ea03f15e9ea9afece622c
Tariok <mingotevan@gmail.com>
2021-12-08 12:09:43 +0100

    modify user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,7 @@
 class UsersController < ApplicationController
     def show
-        
+        @user = current_user
+        @user_email = current_user.email
     end
-    def edit
-        
-    end
-    def update
-        
-    end
+
 end
