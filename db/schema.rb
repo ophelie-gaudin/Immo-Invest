262843f21b9b857ce93d096f9f41252630355233
Istarengwa <muller.lilian@outlook.fr>
2021-12-08 15:45:35 +0100

    installation active storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_07_104402) do
+ActiveRecord::Schema.define(version: 2021_12_08_141520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "housings", force: :cascade do |t|
     t.integer "ad_price"
@@ -33,20 +54,17 @@ ActiveRecord::Schema.define(version: 2021_12_07_104402) do
     t.boolean "rental_unpayment_insurance"
     t.integer "building_co_tax"
     t.integer "maintenance_percentage"
-    t.bigint "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["project_id"], name: "index_housings_on_project_id"
   end
 
   create_table "projects", force: :cascade do |t|
+    t.integer "id_user"
     t.string "title"
     t.string "localization"
     t.string "comment"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,4 +80,5 @@ ActiveRecord::Schema.define(version: 2021_12_07_104402) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
