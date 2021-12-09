15536eff69d665166b95ef89b4e6dda3063d1b12
Istarengwa <muller.lilian@outlook.fr>
2021-12-09 12:21:10 +0100

    add button path admin pannel in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
   resource 'projects', only:[:show]
   
   scope 'admin', module: 'admin', as: 'admin' do
-    resources :pannel
+    resource :pannel
 
   end
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "statics#home"
