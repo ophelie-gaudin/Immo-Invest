700cb28480b300501b174c58af555949a4ddef54
Tariok <mingotevan@gmail.com>
2021-12-10 17:49:00 +0100

    add btn + remove housings of a project then destroy pj

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,63 +1,61 @@
 class ProjectsController < ApplicationController
    # before_action :set_user_project, only: [:show]
     
-    def index
+  def index
+    @projects = Project.where(user_id: current_user.id) 
+  end
 
-    @projects = Project.where(user_id: current_user.id)    
-        
-    end
+  def new
+    @project = Project.new
+  end
 
-    def new
-        @project = Project.new
-    end
-
-    def show
-        @project = Project.find(params[:id])
-        @project_title = @project.title 
-        @project_localization = @project.localization 
-        @project_comment = @project.comment
-        @project_id = @project.user_id.to_i
-
-        @housings = Housing.where(project_id: params[:id])
-      
-        
-    end
+  def show
+    @project = Project.find(params[:id])
+    @project_title = @project.title 
+    @project_localization = @project.localization 
+    @project_comment = @project.comment
+    @project_id = @project.user_id.to_i
+    @housings = Housing.where(project_id: params[:id]) 
+  end
 
 
-    def create
-        @project = Project.create(
-        title: params[:project][:title],
-        comment: params[:project][:comment],
-        localization: params[:project][:localization],
-        user_id: current_user.id
-        )
-        redirect_to projects_path
-    end
+  def create
+    @project = Project.create(
+    title: params[:project][:title],
+    comment: params[:project][:comment],
+    localization: params[:project][:localization],
+    user_id: current_user.id
+    )
+    redirect_to projects_path
+  end
 
 
   def destroy 
     @project = Project.find(params[:id])
+    @project_housings = Housing.where(project_id: @project.id)
+    @project_housings.each do |project|
+      project.destroy
+    end
     @project.destroy
-    redirect_to project_housing_path
+    redirect_to projects_path
   end 
 
   def edit
     
     @project = Project.find(params[:id])
 
-    end
   end
 
+  private
 
-    private
+  def set_user_project
+    @project_user = Project.find(params[:id].to_i).user_id
 
-    def set_user_project
-        @project_user = Project.find(params[:id].to_i).user_id
-
-        if @project_user.to_i == current_user.id
-        else
-            redirect_to root_path
-        end
+    if @project_user.to_i == current_user.id
+    else
+      redirect_to root_path
+    end
+  end
         
 end
 
