5e7bc5bb1b32086c8d7ac7f9d68e388d88ec53eb
Mariem <Mariem2708@gmail.com>
2021-12-15 19:33:36 +0100

    modif to navbar and footer

--- app/models/housing.rb
+++ app/models/housing.rb
@@ -1,7 +1,8 @@
 class Housing < ApplicationRecord
   belongs_to :project
   has_one :user, through: :project
-  has_many_attached :pictures
+  has_many_attached :pictures 
+
   validates :ad_price, presence: true
   validates :property_category, presence: true
   validates :localization, presence: true
